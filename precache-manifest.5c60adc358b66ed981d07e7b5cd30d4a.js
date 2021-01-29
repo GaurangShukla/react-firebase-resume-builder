8d56ee15c31b8913b5e5ae47e87dce66f9b390e0
GaurangShukla <99.22.shukla.gaurang@gmail.com>
2021-01-29 15:09:24 +0530

    static files upload

---
+++ precache-manifest.5c60adc358b66ed981d07e7b5cd30d4a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70ccfd959cd6ed33c49832c4ca9b046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67dd7b33639a20fd5391",
+    "url": "/static/css/3.61c7aef1.chunk.css"
+  },
+  {
+    "revision": "f52abafcd3d0af29adb5",
+    "url": "/static/css/main.cddccbb7.chunk.css"
+  },
+  {
+    "revision": "094e54cb2fcdc5520271",
+    "url": "/static/js/2.f61f5bcc.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.f61f5bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67dd7b33639a20fd5391",
+    "url": "/static/js/3.491f0ae6.chunk.js"
+  },
+  {
+    "revision": "fb8224a48979facf8e6d698936d418ac",
+    "url": "/static/js/3.491f0ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c3e3d0954177e1aa27",
+    "url": "/static/js/4.80eb4ea1.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.80eb4ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfb0a976a9d0a3af9c4",
+    "url": "/static/js/5.e53f1c30.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.e53f1c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52abafcd3d0af29adb5",
+    "url": "/static/js/main.7b2cd49e.chunk.js"
+  },
+  {
+    "revision": "cda7041fad7e2459e9ae",
+    "url": "/static/js/runtime-main.854dfcf5.js"
+  },
+  {
+    "revision": "0c571bfbfb5814de7c592683bb268a90",
+    "url": "/static/media/1.0c571bfb.JPG"
+  },
+  {
+    "revision": "fba178679a94a1993bfda27c5edda53d",
+    "url": "/static/media/2.fba17867.JPG"
+  },
+  {
+    "revision": "776a571459dad3cf206dbbb58b1f4775",
+    "url": "/static/media/3.776a5714.JPG"
+  },
+  {
+    "revision": "af60bc02e18c70a997d54451f8638c8c",
+    "url": "/static/media/4.af60bc02.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "1b5120a51a8a7b5a2bbb88ead0088cd0",
+    "url": "/static/media/address.1b5120a5.svg"
+  },
+  {
+    "revision": "4891f098d240d8fef3a6082796d096c3",
+    "url": "/static/media/envelope.4891f098.svg"
+  },
+  {
+    "revision": "f57fd85218c3874da537e524a3a80711",
+    "url": "/static/media/phone-call.f57fd852.svg"
+  },
+  {
+    "revision": "5f4c69ad8a5a2bdff5c944577d530221",
+    "url": "/static/media/preview.5f4c69ad.png"
+  },
+  {
+    "revision": "9082c49f388c746aa2c6ac98be3f39f8",
+    "url": "/static/media/undraw_add_document_0hek.9082c49f.svg"
+  }
+]);
